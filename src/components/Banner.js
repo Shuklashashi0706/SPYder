b03aeacdf9e8e11dd1b2a7d5aef086be0d679c08
Shuklashashi0706 <shuklashashi169@gmail.com>
2023-05-28 14:01:01 +0530

    react-typed uninstall

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Typed from "react-typed";
+// import Typed from "react-typed"; //abhi ke liye react-type delete kr rha hu baad me install krna he
 function Banner() {
   return (
     <div className="bg-[#2699fb] py-[100px] w-full text-center font-bold">
@@ -8,9 +8,9 @@ function Banner() {
       </div>
       <h1 className=" text-[#00df9a] text-4xl md:text-7xl">Grow with us.</h1>
       <div className=" text-xl md:text-3xl text-white mt-4 ">
-        Learn
-        <Typed className="pl-[4px]"
-        strings={["Cyber Security","Ethical Hacking","Penetration Testing"]} typeSpeed={100} backSpeed={50} loop={true} />
+        Learn Cybersecurity
+        {/* <Typed className="pl-[4px]"
+        strings={["Cyber Security","Ethical Hacking","Penetration Testing"]} typeSpeed={100} backSpeed={50} loop={true} /> */}
       </div>
       <button className='bg-black text-white rounded-md p-3 px-4 mt-3 hover:scale-110'>Get Started</button>
     </div>
